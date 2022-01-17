02ee402c8873a30fe95e42ee913e22f62dd84f46
Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>
2022-01-17 18:15:48 +0100

    feat: ResnetGenerator model fix
    Signed-off-by: Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>

--- src/resnet_generator.py
+++ src/resnet_generator.py
@@ -1,30 +1,41 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from downsampling_block import DownsamplingBlock
+from reflection_padding_layer import ReflectionPadding2D
+from residual_block import ResidualBlock
+from upsampling_block import UpsamplingBlock
+
+import tensorflow_addons as tfa
+
 class ResnetGenerator(keras.Model):
-    def __init__(self, filters = 64, nb_downsampling_blocks = 2, nb_residual_blocks = 9,
-                  nb_upsampling_blocks = 2, gamma_initializer = gamma_init, name = None):
+    def __init__(self, input_size, filters = 64, nb_downsampling_blocks = 2, nb_residual_blocks = 9, nb_upsampling_blocks = 2,
+                 kernel_initializer = keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02),
+                 gamma_initializer = keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02), name = None):
         super(ResnetGenerator, self).__init__()
 
-        input_layer = layers.Input(shape = input_img_size, name = name + "_img_input")
-        input_reflection_layer = ReflectionPadding2D(padding = (3, 3))(input_layer)
-        input_conv_layer = layers.Conv2D(filters, (7, 7), kernel_initializer = kernel_init, use_bias = False)(input_reflection_layer)
-        norm_layer = tfa.layers.InstanceNormalization(gamma_initializer = kernel_init)(input_conv_layer)
-        previous_layer = layers.Activation('relu')(norm_layer)
+        input_tensor = layers.Input(shape = input_size, name = name + "_img_input")
+        input_reflection_tensor = ReflectionPadding2D(padding = (3, 3))(input_tensor)
+        input_conv_tensor = layers.Conv2D(filters, (7, 7), kernel_initializer = kernel_initializer, use_bias = False)(input_reflection_tensor)
+        norm_tensor = tfa.layers.InstanceNormalization(gamma_initializer = gamma_initializer)(input_conv_tensor)
+        tensor = layers.Activation('relu')(norm_tensor)
 
         for _ in range(nb_downsampling_blocks):
             filters *= 2
-            previous_layer = downsample(previous_layer, filters = filters, activation = layers.Activation('relu'))
+            tensor = DownsamplingBlock(filters, activation = layers.Activation('relu'))(tensor)
 
         for _ in range(nb_residual_blocks):
-            previous_layer = residual_block(previous_layer, activation = layers.Activation('relu'))
+            tensor = ResidualBlock(256, activation = layers.Activation('relu'))(tensor)
 
         for _ in range(nb_upsampling_blocks):
             filters //= 2
-            previous_layer = upsample(previous_layer, filters, activation = layers.Activation('relu'))
+            tensor = UpsamplingBlock(filters, activation = layers.Activation('relu'))(tensor)
 
-        output_reflection_layer = ReflectionPadding2D(padding = (3, 3))(previous_layer)
-        output_conv_layer = layers.Conv2D(3, (7, 7), padding = 'valid')(output_reflection_layer)
-        activation_layer = layers.Activation('tanh')(output_conv_layer)
+        output_reflection_tensor = ReflectionPadding2D(padding = (3, 3))(tensor)
+        output_conv_tensor = layers.Conv2D(3, (7, 7), padding = 'valid')(output_reflection_tensor)
+        activation_tensor = layers.Activation('tanh')(output_conv_tensor)
 
-        self.model = keras.models.Model(input_layer, activation_layer, name = name)
+        self.model = keras.models.Model(input_tensor, activation_tensor, name = name)
 
-    def call(self, input, training = False):
-        return self.model(input, training)
+    def call(self, input_tensor, training = False):
+        return self.model(input_tensor, training)
