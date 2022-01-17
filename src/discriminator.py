63c246549545030515d865109c5cfe642f7efa28
Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>
2022-01-17 18:23:27 +0100

    feat: Discriminator model fix
    Signed-off-by: Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>

--- src/discriminator.py
+++ src/discriminator.py
@@ -4,10 +4,11 @@ from tensorflow.keras import layers
 from downsampling_block import DownsamplingBlock
 
 class Discriminator(keras.Model):
-    def __init__(self, filters = 64, kernel_initializer = keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02),
-                    nb_downsampling_blocks = 3, name = None):
+    def __init__(self, input_size, filters = 64, nb_downsampling_blocks = 3,
+                    kernel_initializer = keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02),
+                    name = None):
         super(Discriminator, self).__init__()
-        input_tensor = layers.Input(shape = input_img_size, name = name + '_img_input')
+        input_tensor = layers.Input(shape = input_size, name = name + '_img_input')
         input_conv_tensor = layers.Conv2D(filters, (4, 4), strides = (2, 2), padding = 'same',
                                           kernel_initializer = kernel_initializer)(input_tensor)
         tensor = layers.LeakyReLU(0.2)(input_conv_tensor)
