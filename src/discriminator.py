03d8c04de02159d2287b5b15198afab07f69f7a3
Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>
2022-01-17 18:16:04 +0100

    feat: Discriminator model fix
    Signed-off-by: Maxime PERRIN-LIVENAIS <maxime.perrin-livenais@epita.fr>

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,22 +1,26 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from downsampling_block import DownsamplingBlock
+
 class Discriminator(keras.Model):
-    def __init__(self, filters = 64, kernel_initializer = kernel_init,
+    def __init__(self, filters = 64, kernel_initializer = keras.initializers.RandomNormal(mean = 0.0, stddev = 0.02),
                     nb_downsampling_blocks = 3, name = None):
         super(Discriminator, self).__init__()
-        input_layer = layers.Input(shape = input_img_size, name = name + '_img_input')
-        input_conv_layer = layers.Conv2D(filters, (4, 4), strides = (2, 2), padding = 'same',
-                                          kernel_initializer = kernel_initializer)(input_layer)
-        previous_layer = layers.LeakyReLU(0.2)(input_conv_layer)
+        input_tensor = layers.Input(shape = input_img_size, name = name + '_img_input')
+        input_conv_tensor = layers.Conv2D(filters, (4, 4), strides = (2, 2), padding = 'same',
+                                          kernel_initializer = kernel_initializer)(input_tensor)
+        tensor = layers.LeakyReLU(0.2)(input_conv_tensor)
         downsampling_filters = filters
         for downsampling_block_index in range(nb_downsampling_blocks):
             downsampling_filters *= 2
             strides = (2, 2) if downsampling_block_index < 2 else (1, 1)
-            previous_layer = downsample(previous_layer, filters = downsampling_filters,
-                                        activation = layers.LeakyReLU(0.2),
-                                        kernel_size = (4, 4), strides = strides)
+            tensor = DownsamplingBlock(downsampling_filters, layers.LeakyReLU(0.2),
+                                        kernel_size = (4, 4), strides = strides)(tensor)
 
-        output_conv_layer = layers.Conv2D(1, (4, 4), strides = (1, 1), padding = 'same',
-                                            kernel_initializer = kernel_initializer)(previous_layer)
-        self.model = keras.models.Model(inputs = input_layer, outputs = output_conv_layer, name = name)
+        output_conv_tensor = layers.Conv2D(1, (4, 4), strides = (1, 1), padding = 'same',
+                                            kernel_initializer = kernel_initializer)(tensor)
+        self.model = keras.models.Model(inputs = input_tensor, outputs = output_conv_tensor, name = name)
 
     def call(self, input, training = False):
         return self.model(input, training)
